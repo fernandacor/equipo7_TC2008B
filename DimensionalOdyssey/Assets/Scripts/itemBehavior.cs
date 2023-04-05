2548a34dd3e78cf872308bcb7ee438f68cbabc00
Fernanda Cantú <fercantu56@gmail.com>
2023-04-05 10:02:15 -0600

    no logre pickUp con P funcional, empece inventario

--- DimensionalOdyssey/Assets/Scripts/itemBehavior.cs
+++ DimensionalOdyssey/Assets/Scripts/itemBehavior.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class itemBehavior : MonoBehaviour
 {
     [SerializeField] InventoryManager.AllItems itemType;
+    // public bool pickUp = false;
+
+    // private void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.P)) //Pick something up
+    //     {
+    //         pickUp = true;
+    //         Debug.Log("Pickup is true");
+    //     }
+    // }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             InventoryManager.Instance.AddItem(itemType);
+            Debug.Log("Item added to inventory");
             Destroy(gameObject);
+            Debug.Log("Item destroyed");
         }
     }
+
 }
