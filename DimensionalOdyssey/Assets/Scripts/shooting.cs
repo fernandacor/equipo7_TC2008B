d8188d70c5dc88ee3daf1eeafc8b37ad3ccc8646
Fernanda Cantú <fercantu56@gmail.com>
2023-04-11 13:22:28 -0600

    solo puede disparar si tiene una pistola

--- DimensionalOdyssey/Assets/Scripts/shooting.cs
+++ DimensionalOdyssey/Assets/Scripts/shooting.cs
@@ -4,24 +4,54 @@ using UnityEngine;
 
 public class shooting : MonoBehaviour
 {
+    [SerializeField] InventoryManager.AllItems requiredItem;
+    public bool canShoot = true;
     public GameObject shootingItem;
     public Transform shootingPoint;
-    public bool canShoot = true;
 
-    private void Update()
+    public bool hasRequiredItem(InventoryManager.AllItems itemRequired)
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if(InventoryManager.Instance._inventoryItems.Contains(itemRequired))
         {
-            Shoot();
+            Debug.Log("hasRequiredItem = true");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Gun"))
+        {
+            if(hasRequiredItem(requiredItem) == true)
+            {
+                Debug.Log("Se tiene el elemento");
+                canShoot = !canShoot;
+                Debug.Log("canShoot = true");
+            }
         }
     }
 
     void Shoot()
     {
-        if(!canShoot)
+        if(canShoot == false)
             return;
-        
+
         GameObject disparable = Instantiate(shootingItem, shootingPoint);
         disparable.transform.parent = null;
+    }
+
+    private void Update()
+    {
+        if (canShoot == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                Shoot();
+            }
+        }
     }
 }
