dc76d1470101f6749746f9839e24bcddd6b7bcfe
Samuel Acevedo <a01026893@tec.mx>
2023-04-13 14:28:54 -0600

    Agregada seccion de historia a la pagina

---
+++ BaseDeDatos/dimensionalOdyssey-data.sql
@@ -0,0 +1,14 @@
+USE dimensionalOdyssey;
+SELECT * FROM dimensionalOdyssey.usuario;
+SELECT * FROM dimensionalOdyssey.armas;
+SELECT * FROM dimensionalOdyssey.partida;
+SELECT * FROM dimensionalOdyssey.tokens;
+SELECT * FROM dimensionalOdyssey.personajes;
+SELECT * FROM dimensionalOdyssey.enemigos;
+SELECT * FROM dimensionalOdyssey.atributos;
+
+INSERT INTO dimensionalOdyssey.usuario VALUES ('samacesan_sam','chami', 'a01026893@tec.mx','Samuel Roberto','Acevedo'), 
+('JpAbuelo', 'jpUwU123', 'a01374091@tec.mx', 'Juan pablo', 'Moreno'), ('fernandacor','ferGameMaster0987','A01782232@tec.mx', 'Fernanda', 'Cantú'),
+('sebas21mg','SebasBb','a01027028@tec.mx','Sebastian','Moncada'),('JpJoven','JpUnU456','A01783127@tec.mx','Juan Pablo', 'Ruiz'),
+('alinarosasm','Aly7u7','A01252720@tec.mx','Alina','Rosas');
+
