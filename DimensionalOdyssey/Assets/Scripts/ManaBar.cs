3e97eadbb73edb453a4fcbb2085a7b2c5c25a7c0
Fernanda Cantú <fercantu56@gmail.com>
2023-04-20 11:45:55 -0600

    script mana bar, no sirve todavia

--- DimensionalOdyssey/Assets/Scripts/ManaBar.cs
+++ DimensionalOdyssey/Assets/Scripts/ManaBar.cs
@@ -5,22 +5,27 @@ using UnityEngine.UI;
 
 public class ManaBar : MonoBehaviour
 {
+    //ManaBar
     public Slider slider;
     public Gradient gradient;
     public Image fill;
 
+    //ManaPoints
+
     public void SetMaxEnergy(int energy)
     {
+        //ManaBar
         slider.maxValue = energy;
         slider.value = energy;
-
         fill.color = gradient.Evaluate(1f);
     }
 
     public void SetEnergy(int energy)
     {
+        //ManaBar
         slider.value = energy;
-
         fill.color = gradient.Evaluate(slider.normalizedValue);
     }
+
+
 }
