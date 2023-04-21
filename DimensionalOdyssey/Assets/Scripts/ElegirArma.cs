69a03f6099d95cd2a06b130afb428ddd1cae12a2
Fernanda Cantú <fercantu56@gmail.com>
2023-04-21 01:11:19 -0600

    interfaz super basica de elegir armas

---
+++ DimensionalOdyssey/Assets/Scripts/ElegirArma.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElegirArma : MonoBehaviour
+{
+    public GameObject cajita;
+    public GameObject pistola;
+    public GameObject escopeta;
+    public GameObject metralladora;
+    public bool options;
+
+    void Start()
+    {
+        // cajita.SetActive(false);
+        pistola.SetActive(false);
+        escopeta.SetActive(false);
+        metralladora.SetActive(false);
+        options = false;
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            cajita.SetActive(false);
+            pistola.SetActive(true);
+            escopeta.SetActive(false);
+            metralladora.SetActive(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            cajita.SetActive(false);
+            pistola.SetActive(false);
+            escopeta.SetActive(true);
+            metralladora.SetActive(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            cajita.SetActive(false);
+            pistola.SetActive(false);
+            escopeta.SetActive(false);
+            metralladora.SetActive(true);
+        }
+    }
+}
