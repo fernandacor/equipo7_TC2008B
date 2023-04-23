35960bc616eb9a52f5581faa678d76e1c62cfcce
Fernanda Cantú <fercantu56@gmail.com>
2023-04-23 14:51:39 -0600

    animacion muerte (falta game over o destroy)

--- DimensionalOdyssey/Assets/Scripts/OnTriggerAnimations.cs
+++ DimensionalOdyssey/Assets/Scripts/OnTriggerAnimations.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class OnTriggerAnimations : MonoBehaviour
 {
     private Animator animator;
+    public CharacterStats characterStats;
 
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Traje")
         {
             animator.SetBool("CambioOutfit", true);
+        }
+    }
+
+    void Update()
+    {
+        if (characterStats.currentHealth <= 0)
+        {
+            animator.SetBool("Death", true);
         }
     }
 }
