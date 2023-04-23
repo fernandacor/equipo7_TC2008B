a5a4302752b4e0b416aaf5a11526b61743081ae1
Sebastian Moncada <sebas.200221@gmail.com>
2023-04-22 23:59:16 -0600

    Depurar códigos

--- DimensionalOdyssey/Assets/Scripts/Mapa/CuartoScript.cs
+++ DimensionalOdyssey/Assets/Scripts/Mapa/CuartoScript.cs
@@ -18,39 +18,7 @@ public class CuartoScript : MonoBehaviour
     public GameObject pasilloL;
 
     public bool descubierto;
-    GameObject sceneManager;
 
-    private GameObject pasillos;
-    // private int anchoPasillos = 10;
-
-    public TileBase paredArriba;
-    // TileBase paredDerecha;
-    // TileBase paredAbajo;
-    // TileBase paredIzquierda;
-    // TileBase esquinaArribaDerecha;
-    // TileBase esquinaArribaIzquierda;
-    // TileBase esquinaAbajoDerecha;
-    // TileBase esquinaAbajoIzquierda;
-    // TileBase piso;
-    // TileBase fondo;
-
-    private void Awake()
-    {
-        // sceneManager = GameObject.Find("SceneManager");
-
-        // paredArriba = sceneManager.GetComponent<SceneManagerScript>().tileParedArriba;
-        // paredDerecha = sceneManager.GetComponent<SceneManagerScript>().tileParedDerecha;
-        // paredAbajo = sceneManager.GetComponent<SceneManagerScript>().tileParedAbajo;
-        // paredIzquierda = sceneManager.GetComponent<SceneManagerScript>().tileParedIzquierda;
-        // esquinaArribaDerecha = sceneManager.GetComponent<SceneManagerScript>().tileEsquinaArribaDerecha;
-        // esquinaArribaIzquierda = sceneManager.GetComponent<SceneManagerScript>().tileEsquinaArribaIzquierda;
-        // esquinaAbajoDerecha = sceneManager.GetComponent<SceneManagerScript>().tileEsquinaAbajoDerecha;
-        // esquinaAbajoIzquierda = sceneManager.GetComponent<SceneManagerScript>().tileEsquinaAbajoIzquierda;
-        // piso = sceneManager.GetComponent<SceneManagerScript>().tilePiso;
-        // fondo = sceneManager.GetComponent<SceneManagerScript>().tileFondo;
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
         cuarto = gameObject.GetComponent<Tilemap>();
@@ -60,23 +28,10 @@ public class CuartoScript : MonoBehaviour
         salidaR = transform.Find("SalidaR").gameObject;
         salidaL = transform.Find("SalidaL").gameObject;
 
-        // pasilloVertical = GameObject.Find("PasilloVertical").GetComponent<Tilemap>();
-        // pasilloHorizontal = GameObject.Find("PasilloHorizontal").GetComponent<Tilemap>();
-
-        // pasillos = GameObject.FindGameObjectWithTag("Aisle");
-
-        // AbrirCuarto();
-        // ObtenerObjeto();
-        AbrirCuarto2();
+        AbrirCuarto();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void AbrirCuarto2()
+    public void AbrirCuarto()
     {
         if (pasilloU == null)
             salidaU.SetActive(true);
@@ -111,8 +66,10 @@ public class CuartoScript : MonoBehaviour
         }
     }
 
-    public void CerrarCuarto2()
+    public void CerrarCuarto()
     {
+        GetComponent<EnemySpawn>().GenerarEnemigos();
+
         salidaU.SetActive(true);
         salidaD.SetActive(true);
         salidaR.SetActive(true);
@@ -127,126 +84,4 @@ public class CuartoScript : MonoBehaviour
         if (pasilloL != null)
             pasilloL.SetActive(false);
     }
-
-    // void ObtenerObjeto()
-    // {
-    //     Vector3 afueraDelCuarto = new Vector3((int)transform.position.x + pasilloVertical.cellBounds.xMax, (int)transform.position.y + cuarto.cellBounds.yMax + 1, 0);
-    //     // Vector3Int afueraDelCuarto = new Vector3Int((int)transform.position.x + pasilloVertical.cellBounds.xMax, (int)transform.position.y, 0);
-    //     // cuarto.SetTile(afueraDelCuarto, paredArriba);
-    //     Collider[] colliders = Physics.OverlapSphere(afueraDelCuarto, 0.1f);
-    //     // Comprobar cada collider para encontrar el objeto correspondiente
-    //     foreach (Collider collider in colliders)
-    //     {
-    //         // Obtener la referencia del objeto a través del collider
-    //         GameObject obj = collider.gameObject;
-
-    //         // Comprobar si la posición del objeto coincide con la posición deseada
-    //         if (obj.transform.position == afueraDelCuarto)
-    //         {
-    //             // El objeto que estamos buscando ha sido encontrado
-    //             Debug.Log("Encontrado el objeto " + obj + " en la posición: " + afueraDelCuarto);
-    //             break;
-    //         }
-    //     }
-    // }
-
-    // public void AbrirCuarto()
-    // {
-    //     Tilemap pisoPasillos = pasillos.transform.Find("Piso").GetComponent<Tilemap>();
-
-    //     AbrirEntradaArriba(pisoPasillos, true);
-    //     AbrirEntradaAbajo(pisoPasillos, true);
-    //     AbrirEntradaDerecha(pisoPasillos, true);
-    //     AbrirEntradaIzquierda(pisoPasillos, true);
-
-    //     pisoPasillos.gameObject.GetComponent<Renderer>().sortingOrder = -1;
-    //     pisoPasillos.SwapTile(fondo, piso);
-    // }
-
-    // public void CerrarCuarto()
-    // {
-    //     Tilemap pisoPasillos = pasillos.transform.Find("Piso").GetComponent<Tilemap>();
-
-    //     AbrirEntradaDerecha(pisoPasillos, false);
-    //     AbrirEntradaIzquierda(pisoPasillos, false);
-    //     AbrirEntradaArriba(pisoPasillos, false);
-    //     AbrirEntradaAbajo(pisoPasillos, false);
-
-    //     pisoPasillos.gameObject.GetComponent<Renderer>().sortingOrder = 1;
-    //     pisoPasillos.SwapTile(piso, fondo);
-
-    //     GetComponent<EnemySpawn>().GenerarEnemigos();
-    //     // Generar enemigos
-    // }
-
-    // void AbrirEntradaArriba(Tilemap pisoPasillos, bool abrir)
-    // {
-    //     Vector3Int afueraDelCuarto = new Vector3Int((int)transform.position.x, (int)transform.position.y + cuarto.cellBounds.yMax + 1, 0);
-    //     Vector3Int posicionPared = new Vector3Int((int)cuarto.cellBounds.center.x - (anchoPasillos / 2), (int)cuarto.cellBounds.yMax - 1, 0);
-
-    //     if (abrir)
-    //         ActivarEntrada(pisoPasillos, afueraDelCuarto, posicionPared, esquinaArribaIzquierda, null, esquinaArribaDerecha, true);
-    //     else
-    //         ActivarEntrada(pisoPasillos, afueraDelCuarto, posicionPared, paredArriba, paredArriba, paredArriba, true);
-    // }
-
-    // void AbrirEntradaAbajo(Tilemap pisoPasillos, bool abrir)
-    // {
-    //     Vector3Int afueraDelCuarto = new Vector3Int((int)transform.position.x, (int)transform.position.y + cuarto.cellBounds.yMin - 1, 0);
-    //     Vector3Int posicionPared = new Vector3Int((int)cuarto.cellBounds.center.x - (anchoPasillos / 2), (int)cuarto.cellBounds.yMin, 0);
-
-    //     if (abrir)
-    //         ActivarEntrada(pisoPasillos, afueraDelCuarto, posicionPared, esquinaAbajoIzquierda, null, esquinaAbajoDerecha, true);
-    //     else
-    //         ActivarEntrada(pisoPasillos, afueraDelCuarto, posicionPared, paredAbajo, paredAbajo, paredAbajo, true);
-    // }
-
-    // void AbrirEntradaDerecha(Tilemap pisoPasillos, bool abrir)
-    // {
-    //     Vector3Int afueraDelCuarto = new Vector3Int((int)transform.position.x + cuarto.cellBounds.xMax + 1, (int)transform.position.y, 0);
-    //     Vector3Int posicionPared = new Vector3Int(cuarto.cellBounds.xMax - 1, (int)cuarto.cellBounds.center.y - (anchoPasillos / 2), 0);
-
-    //     if (abrir)
-    //         ActivarEntrada(pisoPasillos, afueraDelCuarto, posicionPared, esquinaAbajoDerecha, null, esquinaArribaDerecha, false);
-    //     else
-    //         ActivarEntrada(pisoPasillos, afueraDelCuarto, posicionPared, paredDerecha, paredDerecha, paredDerecha, false);
-    // }
-
-    // void AbrirEntradaIzquierda(Tilemap pisoPasillos, bool abrir)
-    // {
-    //     Vector3Int afueraDelCuarto = new Vector3Int((int)transform.position.x + cuarto.cellBounds.xMin - 1, (int)transform.position.y, 0);
-    //     Vector3Int posicionPared = new Vector3Int(cuarto.cellBounds.xMin, (int)cuarto.cellBounds.center.y - (anchoPasillos / 2), 0);
-
-    //     if (abrir)
-    //         ActivarEntrada(pisoPasillos, afueraDelCuarto, posicionPared, esquinaAbajoIzquierda, null, esquinaArribaIzquierda, false);
-    //     else
-    //         ActivarEntrada(pisoPasillos, afueraDelCuarto, posicionPared, paredIzquierda, paredIzquierda, paredIzquierda, false);
-    // }
-
-    // void ActivarEntrada(Tilemap pisoPasillos, Vector3Int afueraDelCuarto, Vector3Int posicionPared, TileBase tileInicio, TileBase tilesMedio, TileBase fin, bool vertical)
-    // {
-
-    //     if (pisoPasillos.GetTile(afueraDelCuarto) != null)
-    //     {
-    //         Vector3Int[] posiciones = new Vector3Int[anchoPasillos];
-    //         TileBase[] tileArray = new TileBase[posiciones.Length];
-
-    //         for (int i = 0; i < posiciones.Length; i++)
-    //         {
-    //             if (vertical)
-    //                 posiciones[i] = cuarto.WorldToCell(cuarto.transform.position) + new Vector3Int(posicionPared.x + i, posicionPared.y, posicionPared.z);
-    //             else
-    //                 posiciones[i] = cuarto.WorldToCell(cuarto.transform.position) + new Vector3Int(posicionPared.x, posicionPared.y + i, posicionPared.z);
-
-    //             if (i == 0)
-    //                 tileArray[i] = tileInicio;
-    //             else if (i == posiciones.Length - 1)
-    //                 tileArray[i] = fin;
-    //             else
-    //                 tileArray[i] = tilesMedio;
-    //         }
-
-    //         transform.Find("Paredes").GetComponent<Tilemap>().SetTiles(posiciones, tileArray);
-    //     }
-    // }
 }
