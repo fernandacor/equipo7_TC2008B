2cdc35070adca29237f12e26f2d44bb521745977
Fernanda Cantú <fercantu56@gmail.com>
2023-04-23 16:03:06 -0600

    create pocion behavior, faltan cosas, idea ya esta

---
+++ DimensionalOdyssey/Assets/Scripts/Stats/PocionBehavior.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PocionBehavior : MonoBehaviour
+{
+    // public CharacterStats characterStats;
+    // public InventoryManager inventoryManager;
+    // [SerializeField] InventoryManager.AllItems itemType;
+
+    // void Start()
+    // {
+    //     characterStats = GameObject.Find("Player").GetComponent<CharacterStats>();
+    //     inventoryManager = GameObject.Find("InventoryManager").GetComponent<InventoryManager>();
+    // }
+
+    //     // pocionVelocidad,
+    //     // pocionEnergia,
+    //     // pocionSuperEnergia,
+    //     // pocionVida,
+    //     // pocionSuperVida,
+    //     // pocionInmunidad,
+    //     // pocionProteccion,
+    //     // pocionIman,
+    //     // pocionMystery
+
+    // public void Update()
+    // {
+    //     if (inventoryManager.pocionVelocidad == true)
+    //     {
+    //         characterStats.velocidadMovimiento +=3;
+    //         pocionVelocidad = false;
+    //     }
+
+    //     if (pocionEnergia == true)
+    //     {
+    //         characterStats.currentMana += 10;
+    //         pocionEnergia = false;
+    //     }
+
+    //     if (pocionSuperEnergia == true)
+    //     {
+    //         characterStats.currentMana = characterStats.maxMana;
+    //         pocionSuperEnergia = false;
+    //     }
+
+    //     if (pocionVida == true)
+    //     {
+    //         characterStats.currentHealth += 10;
+    //         pocionVida = false;
+    //     }
+
+    //     if (inventoryManager.pocionSuperVida == true)
+    //     {
+    //         characterStats.currentHealth = characterStats.maxHealth;
+    //         inventoryManager.pocionSuperVida = false;
+    //     }
+    // }
+}
