f2fff7a3987f56fc6caf2b80f33ad387a9dc89bc
Juan Pablo Moreno <jpmoreno98@gmail.com>
2023-04-26 13:16:09 -0600

    Agregando shop manager y panels

--- DimensionalOdyssey/Assets/Scripts/UI/ShopItem.cs
+++ DimensionalOdyssey/Assets/Scripts/UI/ShopItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ShopMenu", menuName = "ShopMenu/ShopItem", order = 1))]
+[CreateAssetMenu(fileName = "ShopMenu", menuName = "Scriptable Objects/New Shop Item", order = 1)]
 public class ShopItem : ScriptableObject
 {
     public string title;
---
+++ DimensionalOdyssey/Assets/Scripts/UI/ShopManagerT.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+public class ShopManagerT : MonoBehaviour
+{
+    public int coins;
+    public TMP_Text coinCounter;
+    public ShopItem[] shopItems;
+    public ShopTemplate[] shopPannels;
+
+    public void LoadPanels(){
+
+        for (int i = 0; ShopItem[i].length; i++)
+        {
+            shopPannels[i].titleText.text = shopItems[i].title;
+            shopPannels[i].priceText.text = shopItems[i].basePrice.ToString();
+        }
+
+    }
+}    
