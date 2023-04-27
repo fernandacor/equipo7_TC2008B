b521a363411353e651a27e8f0e5b0c88e32c8e7b
Juan Pablo Moreno <jpmoreno98@gmail.com>
2023-04-27 15:48:28 -0600

    haciendo el menu de pausa

---
+++ DimensionalOdyssey/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // public static bool GameIsPaused = false;//variable para saber si el juego esta pausado
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Escape))//si se presiona escape
+    //     {
+    //         if (GameIsPaused)//si el juego esta pausado
+    //         {
+    //             Resume();//se reanuda el juego
+    //         }
+    //         else
+    //         {
+    //             Pause();//se pausa el juego
+    //         }
+    //     }
+    // }
+}
