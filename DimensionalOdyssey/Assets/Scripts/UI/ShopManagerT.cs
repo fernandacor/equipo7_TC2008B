670754a8d73279fce51b592f8160d2f62da91a07
Alina Rosas <alinarosasm@gmail.com>
2023-04-27 10:08:58 -0600

    Editar mapa

--- DimensionalOdyssey/Assets/Scripts/UI/ShopManagerT.cs
+++ DimensionalOdyssey/Assets/Scripts/UI/ShopManagerT.cs
@@ -1,53 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using TMPro;
 
-public class ShopManagerT : MonoBehaviour
-{
-    public ShopItem[] shopItem;
-    public ShopTemplate[] shopPanel;
-    public GameObject[] shopPanelGO;
-    public Button[] myPurchaseBtn;
+// public class ShopManagerT : MonoBehaviour
+// {
+//     public ShopItem[] shopItem;
+//     public ShopTemplate[] shopPanel;
+//     public GameObject[] shopPanelGO;
+//     public Button[] myPurchaseBtn;
 
-    void Start()
-    {
-        for (int i = 0; i < shopItem.Length; i++){
-            shopPanelGO[i].SetActive(true);
-        }
-        LoadPanels();
-        CheckPurchasable();
-    }
+//     void Start()
+//     {
+//         for (int i = 0; i < shopItem.Length; i++){
+//             shopPanelGO[i].SetActive(true);
+//         }
+//         LoadPanels();
+//         CheckPurchasable();
+//     }
 
-    void Update()
-    {
-        // code to update coin counter
-    }
+//     void Update()
+//     {
+//         // code to update coin counter
+//     }
 
-    public void CheckPurchasable(){
-    for (int i = 0; i < shopItem.Length; i++){
-        if (coins >= shopItem[i].basePrice)
-            myPurchaseBtn[i].interactable = true;
-        else
-            myPurchaseBtn[i].interactable = false;
-        }
-    }
+//     public void CheckPurchasable(){
+//     for (int i = 0; i < shopItem.Length; i++){
+//         if (coins >= shopItem[i].basePrice)
+//             myPurchaseBtn[i].interactable = true;
+//         else
+//             myPurchaseBtn[i].interactable = false;
+//         }
+//     }
 
-    public void PurchaseItem(int btnNo){
-        if (coins >= shopItem[btnNo].basePrice){
-            coins -= shopItem[btnNo].basePrice;
-            shopItem[btnNo].quantity++;
-            CheckPurchasable();
-        }
-    }
+//     public void PurchaseItem(int btnNo){
+//         if (coins >= shopItem[btnNo].basePrice){
+//             coins -= shopItem[btnNo].basePrice;
+//             shopItem[btnNo].quantity++;
+//             CheckPurchasable();
+//         }
+//     }
 
-    public void LoadPanels()
-    {
-        for (int i = 0; i < shopItem.Length; i++)
-        {
-            shopPanel[i].titleText.text = shopItem[i].title;
-            shopPanel[i].priceText.text = shopItem[i].basePrice.ToString();
-        }
-    }
-}
+//     public void LoadPanels()
+//     {
+//         for (int i = 0; i < shopItem.Length; i++)
+//         {
+//             shopPanel[i].titleText.text = shopItem[i].title;
+//             shopPanel[i].priceText.text = shopItem[i].basePrice.ToString();
+//         }
+//     }
+// }
