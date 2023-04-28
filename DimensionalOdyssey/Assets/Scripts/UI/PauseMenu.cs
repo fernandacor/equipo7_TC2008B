ece6b95737b2e51e5970f4ff93943e8740a63e8e
Juan Pablo Moreno <jpmoreno98@gmail.com>
2023-04-27 18:26:38 -0600

    Logica del PauseMenu

--- DimensionalOdyssey/Assets/Scripts/UI/PauseMenu.cs
+++ DimensionalOdyssey/Assets/Scripts/UI/PauseMenu.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // public static bool GameIsPaused = false;//variable para saber si el juego esta pausado
+    public static bool GameIsPaused = false;//variable para saber si el juego esta pausado
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.Escape))//si se presiona escape
-    //     {
-    //         if (GameIsPaused)//si el juego esta pausado
-    //         {
-    //             Resume();//se reanuda el juego
-    //         }
-    //         else
-    //         {
-    //             Pause();//se pausa el juego
-    //         }
-    //     }
-    // }
+    public GameObject pauseMenu;//menu de pausa
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))//si se presiona escape
+        {
+            if (GameIsPaused)//si el juego esta pausado
+            {
+                 Resume();//se reanuda el juego
+            }
+            else
+            {
+                 Pause();//se pausa el juego
+            }
+        }
+    }
+
+    public void Resume()//funcion para reanudar el juego
+    {
+        pauseMenu.SetActive(false);//se desactiva el menu de pausa
+        Time.timeScale = 1f;//se reanuda el tiempo
+        GameIsPaused = false;//se cambia el estado del juego
+    }
+
+    void Pause()//funcion para pausar el juego
+    {
+        pauseMenu.SetActive(true);//se activa el menu de pausa
+        Time.timeScale = 0f;//se pausa el tiempo
+        GameIsPaused = true;//se cambia el estado del juego
+    }
 }
