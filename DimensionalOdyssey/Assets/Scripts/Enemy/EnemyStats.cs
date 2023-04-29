a83624d13c894c5e8f63b18f2967c45c53974760
Fernanda Cantú <fercantu56@gmail.com>
2023-04-29 10:21:29 -0600

    comentar scripts dentro de carpeta enemy

--- DimensionalOdyssey/Assets/Scripts/Enemy/EnemyStats.cs
+++ DimensionalOdyssey/Assets/Scripts/Enemy/EnemyStats.cs
@@ -1,26 +1,31 @@
 using UnityEngine;
 
+// Script que define las estadisticas de los enemigos
+
 public class EnemyStats : MonoBehaviour
 {
+    // Variables de salud maxima y salud actual
     public int maxHealth = 100;
     public int currentHealth;
 
-    public float resistencia;
+    // Variables para velocidad de movimiento
     public float velocidadMovimiento;
-    public float velocidadDisparo;
-    public float robodeVida;
 
+    // Buscar el animador
     public Animator animator;
 
     void Start()
     {
+        // Se define la salud inicial como la salud maxima
         currentHealth = maxHealth;
     }
 
     void TakeDamage(int damage)
     {
+        // Se le resta el daño a la salud del enemigo
         currentHealth -= damage;
 
+        // Si la salud del enemigo es menor o igual a 0, se destruye
         if (currentHealth <= 0)
         {
             Destroy(gameObject, 2);
@@ -29,9 +34,10 @@ public class EnemyStats : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") || collision.CompareTag("Players Bullet"))
+        // Si una bala del jugador colisiona con el enemigo, el enemigo recibe daño
+        if (collision.CompareTag("Players Bullet"))
         {
-            TakeDamage(10);
+            TakeDamage(characterStats.dañoInfligido);
         }
     }
 }
