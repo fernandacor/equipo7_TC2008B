8d4baedf08a026cba11da3152f752b424eac2ad3
Fernanda Cantú <fercantu56@gmail.com>
2023-04-29 16:35:23 -0600

    comentar itembehavior y ontriggeranimations

--- DimensionalOdyssey/Assets/Scripts/Items/ItemBehavior.cs
+++ DimensionalOdyssey/Assets/Scripts/Items/ItemBehavior.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Script para definir el comportamiento de los items
+
 public class ItemBehavior : MonoBehaviour
 {
+    // Se busca el inventario y la lista de todo 
     [SerializeField] InventoryManager.AllItems itemType;
+    
+    // Booleano que detecta si el objeto puede ser recogido o no
     public bool pickUp = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Si el trigger detecta al jugador, el objeto se vuelve recogibles
         if (collision.gameObject.tag == "Player")
         {
             pickUp = true;
@@ -17,6 +23,7 @@ public class ItemBehavior : MonoBehaviour
 
     private void Update()
     {
+        // Cuando el jugador pica E, recoge el item y se registra en el inventario
         if (Input.GetKeyDown(KeyCode.E) && pickUp == true)
         {
             InventoryManager.Instance.AddItem(itemType);
@@ -26,6 +33,7 @@ public class ItemBehavior : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        // Si el jugador sale del trigger, el item deja de ser recogible
         if (collision.gameObject.tag == "Player")
         {
             pickUp = false;
--- DimensionalOdyssey/Assets/Scripts/MainCharacter/OnTriggerAnimations.cs
+++ DimensionalOdyssey/Assets/Scripts/MainCharacter/OnTriggerAnimations.cs
@@ -4,40 +4,49 @@ using UnityEngine;
 
 public class OnTriggerAnimations : MonoBehaviour
 {
+    // Elementos a buscar
     private Animator animator;
     private CharacterStats characterStats;
+    private GameObject trajeInicial;
 
     void Start()
     {
+        // Se buscan los elementos
         animator = GetComponent<Animator>();
         characterStats = GetComponent<CharacterStats>();
+        trajeInicial = GameObject.FindGameObjectWithTag("Traje");
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        // Si el jugador choca con el objeto "Traje", se activa la animación de cambio de outfit
         if (collision.gameObject.tag == "Traje")
         {
+            Destroy(trajeInicial);
+            Debug.Log("Traje destruido");
             animator.SetBool("CambioOutfit", true);
         }
 
-        if(collision.gameObject.tag == "Pistola")
-        {
-            animator.SetBool("Pistola", true);
-        }
+        // Dependiendo de que pistola se tenga, se cambia la animación
+        // if(collision.gameObject.tag == "Pistola")
+        // {
+        //     animator.SetBool("Pistola", true);
+        // }
 
-        if(collision.gameObject.tag == "Escopeta")
-        {
-            animator.SetBool("Escopeta", true);
-        }
+        // if(collision.gameObject.tag == "Escopeta")
+        // {
+        //     animator.SetBool("Escopeta", true);
+        // }
 
-        if(collision.gameObject.tag == "Metralleta")
-        {
-            animator.SetBool("Metralleta", true);
-        }
+        // if(collision.gameObject.tag == "Metralleta")
+        // {
+        //     animator.SetBool("Metralleta", true);
+        // }
     }
 
     void Update()
     {
+        // Si la salud del jugador es igual o menor a cero, se activa la animación de muerte
         if (characterStats.currentHealth <= 0)
         {
             animator.SetBool("Death", true);
