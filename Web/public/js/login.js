8d1cc048c390e529a3e3063772cc19194d0d24a7
Samuel Acevedo <a01026893@tec.mx>
2023-04-30 17:20:51 -0600

    Ya te dice si el usuario no existe o si la contraseña es incorrecta, solo falta terminarlo con un diseño más bonito

--- Web/public/js/login.js
+++ Web/public/js/login.js
@@ -1,97 +1,7 @@
-/*document.addEventListener('DOMContentLoaded', async () => {
-    const form = document.getElementById('formInput');
-    if (form) {
-        form.onsubmit = async (e) => {
-        e.preventDefault();
-
-        const data = new FormData(e.target);
-        const dataObj = Object.fromEntries(data.entries());
-
-        let response = await fetch('/api/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(dataObj)
-        });
-
-        if (response.ok) {
-            let results = await response.json();
-            console.log(results);
-            window.location.href = results.redirect;
-        } else {
-            console.log(response.status);
-        }
-        };
-    }
-});
-
-window.addEventListener('load', async () => {
-    // Verificar si el usuario ha iniciado sesión en el backend
-    let response = await fetch('/api/session');
-    const process = document.getElementById('process');
-    if (response.ok) {
-        let user = await response.json();
-        // Si el usuario ha iniciado sesión, mostrar el enlace de "Hola" en lugar del enlace de inicio de sesión
-        const userLink = document.getElementById('user-link');
-        const userName = document.getElementById('user-name');
-        userName.textContent = user.username;
-        //userLink.href = '/profile';
-        //document.getElementById('loginLink').style.display = "none";
-        //document.getElementById("user-info").style.display = "block";
-  } else {
-        // Si el usuario no ha iniciado sesión, mostrar el enlace de inicio de sesión en lugar del enlace de "Hola"
-        process.textContent = "Log In";
-        process.style.display = "block";
-        //document.getElementById('user-info').style.display = "none";
-        //document.getElementById('loginLink').style.display = "block";
-  }
-  });*/
-/*
-  document.addEventListener('DOMContentLoaded', async () => {
-    const form = document.getElementById('formInput');
-    if (form) {
-        form.onsubmit = async (e) => {
-            e.preventDefault();
-
-            const data = new FormData(e.target);
-            const dataObj = Object.fromEntries(data.entries());
-
-            let response = await fetch('/api/login', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(dataObj)
-            });
-
-            if (response.ok) {
-                let results = await response.json();
-                console.log(results);
-                window.location.href = results.redirect;
-            } else {
-                console.log(response.status);
-            }
-        };
-    }
-
-    // Verificar si el usuario ha iniciado sesión en el backend
-    let response = await fetch('/api/session');
-    if (response.ok) {
-        let user = await response.json();
-        // Si el usuario ha iniciado sesión, mostrar el enlace de "Hola" en lugar del enlace de inicio de sesión
-        const userLink = document.getElementById('user-link');
-        const userName = document.getElementById('user-name');
-        userName.textContent = user.username;
-        userLink.href = '/profile';
-        document.getElementById('loginLink').style.display = "none";
-        document.getElementById("user-info").style.display = "block";
-  } else if (!response.ok){
-        // Si el usuario no ha iniciado sesión, mostrar el enlace de inicio de sesión en lugar del enlace de "Hola"
-        document.getElementById('user-info').style.display = "none";
-        document.getElementById('loginLink').style.display = "block";
-  }
-});
-*/
-
 document.addEventListener('DOMContentLoaded', async () => {
     const form = document.getElementById('formInput');
+    const loginError = document.getElementById('login-error');
+
     if (form) {
       form.onsubmit = async (e) => {
         e.preventDefault();
@@ -110,11 +20,14 @@ document.addEventListener('DOMContentLoaded', async () => {
           console.log(results);
           window.location.href = results.redirect;
         } else {
-          console.log(response.status);
+            console.log(response.status);
+            let errorResponse = await response.json();
+            loginError.textContent = errorResponse.message;
+            loginError.style.display = 'block';
         }
       };
     }
-  });
+});
   
 window.addEventListener('load', async () => {
     // Verificar si el usuario ha iniciado sesión en el backend
@@ -140,4 +53,4 @@ window.addEventListener('load', async () => {
       loginLink.style.display = 'block';
       userGreeting.style.display = 'none';
     }
-});
+}); 
