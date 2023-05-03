dc2092d2806fb628eaddb4b9cf227cde4eddfdbe
Juan Pablo Moreno <jpmoreno98@gmail.com>
2023-05-03 10:30:06 -0600

    Ultimo commit jp

--- DimensionalOdyssey/Assets/Scripts/UI/ToolBarItem.cs
+++ DimensionalOdyssey/Assets/Scripts/UI/ToolBarItem.cs
@@ -6,40 +6,29 @@ using UnityEngine.UI;
 
 public class ToolBarItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public Item item;
-    
-    [Header("UI")]
-    private Image image;
+    Image image;
 
-    [HideInInspector] public Transform parentAfterDrag;
+[HideInInspector] public Transform parentAfterDrag;
 
-    private void Start(){
-        //IntialiseItem(item);    
-    }
+public void OnBeginDrag(PointerEventData eventData)
+{
+    Debug.Log("Begin Drag");
+    image.raycastTarget = false;
+    parentAfterDrag = transform.parent;
+    transform.SetParent(transform.root);
+    transform.SetAsLastSibling();
+}
 
-    // public void IntialiseItem(Item newItem){
-    //     image.sprite = newItem.icon;
-    // }
+public void OnDrag(PointerEventData eventData)
+{
+    Debug.Log("Dragging");
+    transform.position = Input.mousePosition;
+}
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("Begin Drag");
-        image.raycastTarget = false;
-        parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root);
-        transform.SetAsLastSibling();
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log("Dragging");
-        transform.position = Input.mousePosition;
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("End Drag");
-        image.raycastTarget = true;
-        transform.SetParent(parentAfterDrag);
-    }
+public void OnEndDrag(PointerEventData eventData)
+{
+    Debug.Log("End Drag");
+    image.raycastTarget = true;
+    transform.SetParent(parentAfterDrag);
+}
 }
