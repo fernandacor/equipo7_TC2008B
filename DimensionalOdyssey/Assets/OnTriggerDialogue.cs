d8ce1d0c7f27f643cda8d20a146e31f375c29025
Fernanda Cantú <fercantu56@gmail.com>
2023-05-02 18:26:10 -0600

    mapa oficina casa

--- DimensionalOdyssey/Assets/OnTriggerDialogue.cs
+++ DimensionalOdyssey/Assets/OnTriggerDialogue.cs
@@ -6,10 +6,32 @@ using UnityEngine.UI;
 public class OnTriggerDialogue : MonoBehaviour
 {
     public GameObject Dialogue;
+    public bool canInteract;
+    public bool itemActive;
+    public GameObject item;
+    public GameObject item2;
 
     void Start()
     {
         Dialogue.SetActive(false);
+        item.SetActive(false);
+        item2.SetActive(false);
+        itemActive = false;
+    }
+
+    void Update()
+    {
+        if (canInteract == true && Input.GetKeyDown(KeyCode.E))
+        {
+            itemActive = true;
+            item.SetActive(true);
+            item2.SetActive(true);
+        }
+        
+        // if (Input.GetKeyDown(KeyCode.E) && canInteract == true && itemActive == true)
+        // {
+        //     item.SetActive(false);
+        // }
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -19,6 +41,19 @@ public class OnTriggerDialogue : MonoBehaviour
             Debug.Log("Player is in the trigger");
             Dialogue.SetActive(true);
             Debug.Log("Dialogue is active");
+            canInteract = true;  
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player is out of the trigger");
+            Dialogue.SetActive(false);
+            Debug.Log("Dialogue is inactive");
+            canInteract = false;
+            item.SetActive(false);
         }
     }
 }
