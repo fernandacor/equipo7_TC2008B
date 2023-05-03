a5f0536fab2da244af4d717d9598f2b78afd0682
Fernanda Cantú <fercantu56@gmail.com>
2023-05-03 00:24:43 -0600

    comentarios en scripts (DNF)

--- DimensionalOdyssey/Assets/Scripts/Items/Interactable.cs
+++ DimensionalOdyssey/Assets/Scripts/Items/Interactable.cs
@@ -1,45 +1,40 @@
 using UnityEngine;
 
-/*	
-	This component is for all objects that the player can
-	interact with such as enemies, items etc. It is meant
-	to be used as a base class.
-*/
+// Script que define el funcionamiento de los objetos con los que se puede interactuar
 
 public class Interactable : MonoBehaviour {
 
-	public float radius = 3f;				// How close do we need to be to interact?
-	public Transform interactionTransform;	// The transform from where we interact in case you want to offset it
+	public float radius = 3f;				// Radio de cercanía necesario para interactuar con el objeto
+	public Transform interactionTransform;	// Transform del objeto
 
-	bool isFocus = false;	// Is this interactable currently being focused?
-	Transform player;		// Reference to the player transform
+	bool isFocus = false;	// Booleano que se activa si el personaje está en el foco del objeto
+	Transform player;		// Referencia al jugador
 
-	bool hasInteracted = false;	// Have we already interacted with the object?
+	bool hasInteracted = false;	// Booleano que se activa si ya se interactuó con un objeto
 
 	public virtual void Interact ()
 	{
-		// This method is meant to be overwritten
+		// Este metodo es virtual para que se pueda sobreescribir en los scripts de los objetos
 		//Debug.Log("Interacting with " + transform.name);
 	}
 
 	void Update ()
 	{
-		// If we are currently being focused
-		// and we haven't already interacted with the object
+		// Función que se llama si el objeto está en foco y si no se ha interactuado con el
 		if (isFocus && !hasInteracted)
 		{
-			// If we are close enough
+			// Si el personaje está suficientemente cerca del objeto:
 			float distance = Vector3.Distance(player.position, interactionTransform.position);
 			if (distance <= radius)
 			{
-				// Interact with the object
+				// Interacción con el objeto
 				Interact();
 				hasInteracted = true;
 			}
 		}
 	}
 
-	// Called when the object starts being focused
+	// Función que se llama cuando el objeto está en el foco
 	public void OnFocused (Transform playerTransform)
 	{
 		isFocus = true;
@@ -47,7 +42,7 @@ public class Interactable : MonoBehaviour {
 		hasInteracted = false;
 	}
 
-	// Called when the object is no longer focused
+	// Función que se llama cuando el objeto deja de estar en el foco
 	public void OnDefocused ()
 	{
 		isFocus = false;
@@ -55,7 +50,7 @@ public class Interactable : MonoBehaviour {
 		hasInteracted = false;
 	}
 
-	// Draw our radius in the editor
+	// Función que dibuja un gizmo en el editor para ver el radio de interacción
 	void OnDrawGizmosSelected ()
 	{
 		if (interactionTransform == null)
--- DimensionalOdyssey/Assets/Scripts/Stats/HealthBar.cs
+++ DimensionalOdyssey/Assets/Scripts/Stats/HealthBar.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Script para el comportamiento de la barra de salud/vida
+
 public class HealthBar : MonoBehaviour
 {
-    //Script para el comportamiento de la barra de salud/vida
+    // Elementos que conforman la barra
     public Slider slider;
     public Gradient gradient;
     public Image fill;
 
+    // Funciones para definir la barra con la salud/vida actual
     public void SetHealth(float health)
     {
         slider.value = health;
         fill.color = gradient.Evaluate(slider.normalizedValue);
     }
     
+    // Funciones para definir la barra con la salud/vida máxima
     public void SetMaxHealth(float health)
     {
         slider.maxValue = health;
